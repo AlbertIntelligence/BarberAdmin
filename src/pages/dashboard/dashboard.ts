cd8da6bb2ed6f192bf7d7091a0cf85c77e494859
MonsieurPetion <lenzpetion@gmail.com>
2017-09-03 06:23:47 -0400

    Binding works

--- src/pages/dashboard/dashboard.ts
+++ src/pages/dashboard/dashboard.ts
@@ -9,31 +9,103 @@ import * as firebase from 'firebase/app';
 })
 export class DashboardComponent {
 
+   idsTicket = [];
+   firstNamesTicket = [];
+   lastNamesTicket = [];
 
-  tableObjects = {
-    'position': '',
-    'userPosition': '',
-    'paymentCompleted': ''
-  }
-  firstName: any = '';
-  lastName: any = '' ;
-  phoneNunber: any = '' ;
+   idsReservation = [];
+   dateReservation = [];
+   timeReservation = [];
+   firstNamesReservation = [];
+   lastNamesReservation = [];
+   numberClientWaitingTicketList = 0;
+   numberClientWaitingReservation = 0;
+
+
   constructor() {
-    this.sendLiveTableData();
+    this.TicketListTable();
+    this.ReservationTable();
+    this.ClientWaiting();
+    this.TotalReservation();
   }
-  // function
-   sendLiveTableData() {
+
+  TicketListTable() {
      const listOfUsers = firebase.database().ref('TicketList/Users/');
-     listOfUsers.on('value', function(snapshot) {
+     listOfUsers.limitToFirst(5).on('value', function(snapshot) {
+       const ids = [];
+       const firstNames = [];
+       const lastNames = [];
        snapshot.forEach(function(childSnapshot) {
-         const childKey = childSnapshot.key;
-         const childData = childSnapshot.val();
-         this.tableObjects.userPosition = childData.firstName;
-         console.log(self);
-         return false;
-       });
-     });
+         const id = childSnapshot.key;
+         const firstName = childSnapshot.val().firstName.toUpperCase();
+         const lastName = childSnapshot.val().lastName.toUpperCase();
+         ids.push(id);
+         firstNames.push(firstName);
+         lastNames.push(lastName);
+       }.bind(this));
+
+
+         this.idsTicket = ids,
+         this.firstNamesTicket = firstNames,
+         this.lastNamesTicket =  lastNames
+
+
+     }.bind(this));
    }
+
+  ReservationTable() {
+    const listOfUsers = firebase.database().ref('Appointments');
+    listOfUsers.limitToFirst(2).on('value', function(snapshot) {
+      const ids = [];
+      const dates = [];
+      const times = [];
+      const firstNames = [];
+      const lastNames = [];
+      snapshot.forEach(function(childSnapshot) {
+        const id = childSnapshot.key;
+        const date = childSnapshot.val().Date.toUpperCase();
+        const time = childSnapshot.val().Hour.toUpperCase();
+        const firstName = childSnapshot.val().firstName.toUpperCase();
+        const lastName = childSnapshot.val().lastName.toUpperCase();
+        ids.push(id);
+        dates.push(date);
+        times.push(time);
+        firstNames.push(firstName);
+        lastNames.push(lastName);
+      }.bind(this));
+
+
+        this.idsReservation = ids,
+        this.dateReservation = dates,
+        this.timeReservation = times,
+        this.firstNamesReservation = firstNames,
+        this.lastNamesReservation =  lastNames
+
+
+    }.bind(this));
+  }
+
+  ClientWaiting() {
+    const listOfUsers = firebase.database().ref('TicketList/Users/');
+    listOfUsers.on('value', function(snapshot) {
+      var numberClientWaitingTicketList = 0;
+      snapshot.forEach(function(childSnapshot) {
+        numberClientWaitingTicketList++;
+      }.bind(this));
+      this.numberClientWaitingTicketList = numberClientWaitingTicketList
+    }.bind(this));
+  }
+
+  TotalReservation() {
+    const listOfUsers = firebase.database().ref('Appointments');
+    listOfUsers.on('value', function(snapshot) {
+      var numberClientWaitingReservation = 0;
+      snapshot.forEach(function(childSnapshot) {
+        numberClientWaitingReservation++;
+      }.bind(this));
+      this.numberClientWaitingReservation = numberClientWaitingReservation
+    }.bind(this));
+  }
 
 
 }
